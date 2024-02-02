00cb3671dc4d76874e3178f826270279c1438017
Giuseppe Longo <longorsm68@gmail.com>
2024-02-02 17:08:15 +0100

    add login validation and controls

--- PHP/day2/gestione.php
+++ PHP/day2/gestione.php
@@ -1,61 +1,158 @@
 <?php
 
-    // print_r($_POST);
-    if($_FILES['fotoFile']['type'] === 'image/jpeg' || $_FILES['fotoFile']['type'] === 'image/png') {
-        // print_r($_FILES['fotoFile']);
-        $file_name = $_FILES['fotoFile']['name'];
-        $senza_spazi = str_replace(' ', '', $file_name);
-        
-        $target_dir = 'uploads/';
-        $tutta_dir = $target_dir . $senza_spazi;
-        // print $tutta_dir;
-        if(is_uploaded_file($_FILES['fotoFile']["tmp_name"]) && $_FILES['fotoFile']["error"] === UPLOAD_ERR_OK) {
-            if(move_uploaded_file($_FILES['fotoFile']['tmp_name'], $tutta_dir)){
-                // echo "mbare tutto appoggio";
-            } else {
-                echo "vez c'è qualcosa che non va";
-            }
+// print_r($_POST);
+if ($_FILES['fotoFile']['type'] === 'image/jpeg' || $_FILES['fotoFile']['type'] === 'image/png') {
+    // print_r($_FILES['fotoFile']);
+    $file_name = $_FILES['fotoFile']['name'];
+    $senza_spazi = str_replace(' ', '', $file_name);
+
+    $target_dir = 'uploads/';
+    $tutta_dir = $target_dir . $senza_spazi;
+    // print $tutta_dir;
+    if (is_uploaded_file($_FILES['fotoFile']["tmp_name"]) && $_FILES['fotoFile']["error"] === UPLOAD_ERR_OK) {
+        if (move_uploaded_file($_FILES['fotoFile']['tmp_name'], $tutta_dir)) {
+            // echo "mbare tutto appoggio";
+        } else {
+            echo "vez c'è qualcosa che non va";
         }
-    } else {?>
+    }
+} else { ?>
     <h3>A zi Me devi da na foto</h3>
-    <?php }
+<?php }
 
 
-    ?>
-    <h3 class="m-3">Dati Utente:</h3>
-    
-    <div class="d-flex row m-3">
-        <div class="col-6">
-            <h4>Name: <span class='fw-light'> <?php echo $_POST['firstname']; ?> </span> </h4>
-            <h4>Surname: <span class='fw-light'> <?php echo $_POST['lastname']; ?> </span> </h4>
-            <h4>Username: <span class='fw-light'> <?php echo $_POST['username']; ?> </span> </h4>
-            <h4>City: <span class='fw-light'> <?php echo $_POST['city']; ?> </span> </h4>
-            <h4>Telephone Number: <span class='fw-light'> <?php echo $_POST['telnumber']; ?> </span> </h4>
-        </div>
-        <div class="col-6">
-            <img class="immagineProfilo" src="<?= $tutta_dir ?>" alt="profile_image">
-        </div>
+
+
+
+
+
+
+?>
+<h3 class="m-3">Dati Utente:</h3>
+
+<div class="d-flex row m-3">
+    <div class="col-6">
+        <h4>Name: <span class='fw-light'>
+                <?php echo $_POST['firstname']; ?>
+            </span> </h4>
+        <h4>Surname: <span class='fw-light'>
+                <?php echo $_POST['lastname']; ?>
+            </span> </h4>
+        <h4>Username: <span class='fw-light'>
+                <?php echo $_POST['username']; ?>
+            </span> </h4>
+        <h4>City: <span class='fw-light'>
+                <?php echo $_POST['city']; ?>
+            </span> </h4>
+        <h4>Telephone Number: <span class='fw-light'>
+                <?php echo $_POST['telnumber']; ?>
+            </span> </h4>
+            <h4>Email: <span class='fw-light'>
+                <?php echo $_POST['mail']; ?>
+            </span> </h4>
+            <h4>Password: <span class='fw-light'>
+                <?php echo $_POST['password']; ?>
+            </span> </h4>
     </div>
-
-    <div class="m-3">
-        <button type="button" class="btn btn-primary" onclick="location.href='index.php'">Back to Home Page</button>
+    <div class="col-6">
+        <img class="immagineProfilo" src="<?= $tutta_dir ?>" alt="profile_image">
     </div>
-    
-    
-    
-    <?php
+</div>
+
+<div class="m-3">
+    <button type="button" class="btn btn-primary" onclick="location.href='index.php'">Back to Home Page</button>
+</div>
+
+
+
+<?php
 
 
 
 
+function printUserData(string $input): void {
+    $data = filter_input(INPUT_POST, $input, FILTER_SANITIZE_STRING);
 
-        session_start(); // inizializza una sessione su browser del client
-        // print_r($_POST);
-        $_SESSION['utenti'][$_POST['username']] = $_POST;
-        $_SESSION['utenti'][$_POST['username']]['foto'] = $tutta_dir;
+    if ($data !== false && preg_match('/^[A-Za-z ]+$/', $data)) {
+        echo "Name: <span class='fw-light'>$data</span> <br/>";
+    } else {
+        echo "Invalid Name";
+        exit();
+    }
+}
 
-        session_write_close();
-        // var_dump($_SESSION);
+printUserData('firstname');
+printUserData('lastname');
+printUserData('city');
+
+function validateAndPrintUsername(string $input): void {
+    $username = filter_input(INPUT_POST, $input, FILTER_SANITIZE_STRING);
+
+    // Può contenere numeri o caratteri speciali
+    // L'espressione regolare consente lettere, numeri, e alcuni caratteri speciali comuni
+    if ($username !== false && preg_match('/^[A-Za-z0-9\-_!@#$%^&*()+=<>?\/.,;:|\\[\]{}]+$/', $username)) {
+        echo "Username: <span class='fw-light'>$username</span> <br/>";
+    } else {
+        echo "Invalid Username";
+        exit();
+    }
+}
+
+// Esempio di utilizzo per $_POST['username']
+validateAndPrintUsername('username');
+
+
+$email = $_POST['mail'];
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "$email è un valore valido <br/>";
+} else {
+    echo "$email  NON è una valore valido";
+}
+
+
+$phoneNumber = $_POST['telnumber'];
+$phoneNumberDigitsOnly = preg_replace('/[^0-9+]/', '', $phoneNumber);
+if (preg_match('/^\+?[0-9]{11}$/', $phoneNumberDigitsOnly) && ((filter_var($phoneNumberDigitsOnly, FILTER_VALIDATE_INT)))) {
+    echo "Valid Phone Number end int";
+} else {
+    echo "Invalid Phone Number";
+    exit();
+} 
+
+
+
+echo $phoneNumberDigitsOnly;
+
+
+function validatePassword(string $password): bool {
+    // La password deve avere almeno 8 caratteri
+    // La password deve contenere almeno una lettera maiuscola
+    // La password deve contenere almeno una lettera minuscola
+    // La password deve contenere almeno un numero
+    // La password può contenere caratteri speciali (modifica se necessario)
+    $pattern = '/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/';
+
+    return preg_match($pattern, $password) === 1;
+}
+
+// Esempio di utilizzo
+$password = $_POST['password'];
+
+if (validatePassword($password)) {
+    echo "Password valida.";
+} else {
+    echo "La password non rispetta i requisiti minimi.";
+    exit();
+}
+
+
+session_start(); // inizializza una sessione su browser del client
+// print_r($_POST);
+$_SESSION['utenti'][$_POST['username']] = $_POST;
+$_SESSION['utenti'][$_POST['username']]['foto'] = $tutta_dir;
+
+session_write_close();
+// var_dump($_SESSION);
 
 
 
@@ -68,23 +165,25 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BackEnd Panel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 </head>
+
 <body>
     <!-- <p>WeWe</p> -->
 
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous">
-    </script>
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+        </script>
 </body>
+
 </html>
--- PHP/day2/register.php
+++ PHP/day2/register.php
@@ -1,23 +1,17 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    <title>Document</title>
+     <link rel="stylesheet" href="style.css">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
+
 </head>
-
 <body>
-    <h1 class="m-3 text-center"> Welcome Back</h1>
-
-    <!-- <form method="POST" class="row g-3 needs-validation m-3" action="gestione.php" enctype="multipart/form-data"
+    
+<form method="POST" class="row g-3 needs-validation m-3" action="gestione.php" enctype="multipart/form-data"
         novalidate>
         <div class="col-md-4">
             <label for="validationCustom01" class="form-label">First name</label>
@@ -57,6 +51,20 @@ session_start();
                 Please provide a valid phone number.
             </div>
         </div>
+        <div class="col-md-3">
+            <label for="validationCustom06" class="form-label">eMail</label>
+            <input type="email" class="form-control" name="mail" placeholder="nome@example.com" required>
+            <div class="invalid-feedback">
+                Please provide a valid email.
+            </div>
+        </div>
+        <div class="col-md-3">
+            <label for="validationCustom06" class="form-label">Password</label>
+            <input type="password" class="form-control" name="password" placeholder="deve contenere 8 caratteri, una lettera maiuscola, un numero " required>
+            <div class="invalid-feedback">
+                Please provide a valid email.
+            </div>
+        </div>
         <div class="mb-3">
             <label for="formFile" class="form-label">Insert your photo (jpeg or png format file)</label>
             <input class="form-control" type="file" name="fotoFile">
@@ -64,61 +72,15 @@ session_start();
 
         <button class="btn btn-primary" type="submit">Submit form</button>
         </div>
-    </form> -->
-
-    <div class="row row-cols-1 row-cols-md-4 g-4 mb-5">
-        <?php
-        if (isset($_SESSION['utenti'])) {
-            foreach ($_SESSION['utenti'] as $key => $value) { ?>
-                <div class="col">
-                    <div class="card">
-                        <img src="<?= $value['foto'] ?>" class="card-img-top img-fluid" style="height: 200px; object-fit: cover;" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title"><?= "@".$key ?></h5>
-                            <p class="card-text"><?= ucfirst(strtolower($value['lastname'])) . " " . ucfirst(strtolower($value['firstname'])) ?></p>
-                            <p class="card-text"><?= $value['city'] ?></p>
-                            <p class="card-text"><small class="text-body-secondary"><?= $value['telnumber'] ?></small></p>
-                        </div>
-                    </div>
-                </div>
-                <?php
-            }
-        }
-
-        ?>
-    </div>
-
-
-
-    <?php
-
-    // session_start();
-    // if(isset($_SESSION['utenti'])) {
-    //     foreach($_SESSION['utenti'] as $key => $value) {
-    //         print ($key."<br>");
-    //         foreach($value as $k => $v) {
-    //             print ($v."<br>");
-    //         }
-    //     }
-    // } else {
-    //     print "non esiste un cazzo";
-    // }
-    
-
-
-
-    ?>
+    </form>
 
 
 
 
 
 
-
-    <!-- bootstrap  -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
         </script>
 </body>
-
 </html>
